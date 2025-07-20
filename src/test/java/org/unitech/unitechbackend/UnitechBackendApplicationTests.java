88fd0959d70a3d282e30c43eae6790d8752790c7
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2025-07-20 14:23:16 +0400

    Structure added

---
+++ src/test/java/org/unitech/unitechbackend/UnitechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.unitech.unitechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
