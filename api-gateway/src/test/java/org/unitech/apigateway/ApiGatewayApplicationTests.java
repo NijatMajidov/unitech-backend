1607200aa06e1942b54ebd31e55dc68f17ad6ebd
abbasova-senuber03 <abbasovasenuber8@gmail.com>
2025-07-21 19:18:54 +0400

    İlk push

--- api-gateway/src/test/java/org/unitech/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/org/unitech/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,19 @@
 package org.unitech.apigateway;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "eureka.client.enabled=false",
+        "spring.data.redis.host=localhost",
+        "spring.data.redis.port=6379"
+})
 class ApiGatewayApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
