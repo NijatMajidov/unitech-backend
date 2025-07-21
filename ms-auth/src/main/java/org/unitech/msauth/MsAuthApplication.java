31521324d5794b550a98b50a1d2d75b8b3a492be
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2025-07-21 15:58:30 +0400

    rabbit, redis, security config and JwtService attached

--- ms-auth/src/main/java/org/unitech/msauth/MsAuthApplication.java
+++ ms-auth/src/main/java/org/unitech/msauth/MsAuthApplication.java
@@ -2,13 +2,16 @@ package org.unitech.msauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MsAuthApplication {
 
     public static void main(String[] args) {
         System.out.println("MsAuthApplication started");
         SpringApplication.run(MsAuthApplication.class, args);
     }
-
 }
