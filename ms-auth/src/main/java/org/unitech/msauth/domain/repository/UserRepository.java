761f5af44e4182a67e985d05d2dd5f454b562b97
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2025-07-22 19:29:52 +0400

    UserRepository fixed

--- ms-auth/src/main/java/org/unitech/msauth/domain/repository/UserRepository.java
+++ ms-auth/src/main/java/org/unitech/msauth/domain/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package org.unitech.msauth.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.unitech.msauth.domain.entity.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
